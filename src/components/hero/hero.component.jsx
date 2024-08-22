7bcb8088af476126e7060d58afe8c929a298e46c
akshkin <akshaya.kini12@gmail.com>
2024-08-22 09:29:42 +0200

    Make hero section better
    Add movie/tv title to hero image, change color of search bar

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -1,32 +1,31 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-import { fetchPopular } from '../../fetchApi'
-import "./hero.styles.scss"
+import { fetchPopular } from "../../fetchApi";
+import "./hero.styles.scss";
 
 function Hero({ option }) {
-  
-  const [hero, setHero] = useState({})
+  const [hero, setHero] = useState({});
 
-  const getRandomNumber = (arr) => Math.ceil(Math.random() * arr.length)
+  const getRandomNumber = (arr) => Math.ceil(Math.random() * arr.length);
 
-  const randomNumber = 0
+  const randomNumber = 0;
 
-  useEffect(() => {   
-    if (option === "movie"){
-      fetchPopular("movie", "now_playing")
-        .then(data => 
-          (setHero(data?.results[randomNumber])))
+  useEffect(() => {
+    if (option === "movie") {
+      fetchPopular("movie", "now_playing").then((data) =>
+        setHero(data?.results[randomNumber])
+      );
+    } else {
+      fetchPopular("tv", "on_the_air").then((data) =>
+        setHero(data?.results[randomNumber])
+      );
     }
-    else {
-      fetchPopular("tv", "on_the_air")
-        .then(data => setHero(data?.results[randomNumber])) 
-    }
-  }, [option])
+  }, [option]);
 
-  const baseImg = 'https://image.tmdb.org/t/p/original'
+  const baseImg = "https://image.tmdb.org/t/p/original";
   // const style ={
-  //   backgroundImage: `url(${baseImg}${hero.backdrop_path})`, 
+  //   backgroundImage: `url(${baseImg}${hero.backdrop_path})`,
   //   backgroundSize: "contain",
   //   backgroundPosition: "center",
   //   backgroundRepeat: "no-repeat",
@@ -36,16 +35,22 @@ function Hero({ option }) {
   //   margin: "0 auto",
   // }
 
-  const { id, title, backdrop_path, overview, name } = hero
+  const { id, title, backdrop_path, overview, name } = hero;
 
   return (
-    <>
-      <h1 className='hero-title'>Now {option === "movie" ? "playing" : "airing"}</h1>
-      <Link to={`/${option}/${id}`} className='hero' >
-        <img src={`${baseImg}${backdrop_path}`} alt={`${title || name} backdrop`}/>
+    <div className="hero">
+      <h1 className="hero-title">
+        Now {option === "movie" ? "playing" : "airing"}
+      </h1>
+      <Link to={`/${option}/${id}`}>
+        <img
+          src={`${baseImg}${backdrop_path}`}
+          alt={`${title || name} backdrop`}
+        />
+        <p className="title">{title || name}</p>
       </Link>
-    </>
-  )
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
